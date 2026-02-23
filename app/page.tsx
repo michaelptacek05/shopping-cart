e932878109e6d7a342807b6781203a0884b2758c
Michael Ptáček <mptacek005@gmail.com>
2026-02-23 19:21:14 +0100

    Fixed package error and added cart styles and images

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@
 export default function Home() {
   return (
     <div>
-       <a href="/products" className="underline text-sky-800 cursor-pointer">Products</a>
-       <br /> 
-       <a href="/cart" className="underline text-sky-800 cursor-pointer">Cart</a>
+      <a href="/products" className="underline text-sky-800 cursor-pointer">Products</a>
+      <br /> 
+      <a href="/cart" className="underline text-sky-800 cursor-pointer">Cart</a>
     </div>
   );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,9 +6,21 @@ export default async function ProductsPage() {
     return (
         <div>
             <h1>Naše produkty</h1>
-            {products.map((product) => (
-                <li key={product.id}>{product.title}</li>
-            ))}
+            <div className="flex flex-wrap gap-5">
+                {products.map((product) => (
+                    <>
+                        <div className="bg-white p-5 border-gray-50 rounded-lg shadow-md" key={product.id}>
+                            <img src={product.image} alt={product.title} className="w-32 h-32 object-cover mb-3" />
+                            <h2>{product.title}</h2>
+                            <p>{product.price}€</p>
+                            <p>{product.description}</p>
+                            <p>{product.category}</p> 
+                            <button>Přidat do košíku</button>
+                        </div>
+                    </>
+                ))}
+            </div>
         </div>
     );
 }
+
